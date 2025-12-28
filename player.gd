ebb3e39499e84b8999af774adec2eea8f831b1d5
Kacper <07Potion@gmail.com>
2025-12-28 17:31:59 +0100

    player moves!

---
+++ player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+signal hit
+
+@export var speed = 200
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	# Must be later, we can';t change physics properties on physics callback WHAT IS A PHYSICS CALLBACK
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$ColissionShape2d.disabled = false
